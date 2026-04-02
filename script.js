001456173e35b2df33aff3b985a645a945c91c25
Ryan Nave <ryannave97@gmail.com>
2026-04-02 10:35:41 -0700

    improved grid logic + implemented 2d array for grid

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-class Player {
-    constructor(name, score, marker) {
-        this.name = name;
-        this.marker = marker;
-    }
+function Player(name, marker){
+    this.name = name;
+    this.marker = marker;
 }
 
 const grid = document.querySelector(".grid-container");
+
+
 let boxes = [];
-for(let i = 0; i < grid.children.length; i++){
-    boxes.push(grid.children[i]);
-    boxes[i].addEventListener('mouseover', () => {
-        boxes[i].style.backgroundColor = 'grey';
-    });
-    boxes[i].addEventListener('mouseover', () => {
-        boxes[i].style.backgroundColor = 'grey';
-    });
+let j = 0;
+for(let i = 0; i < 3; i++){
+    let boxRow = [];
+    for(let k = 0; k < 3; k++){
+        console.log(j);
+        boxRow.push(grid.children[j])
+        j++;
+    }
 }
+boxes.forEach(r => {
+    r.forEach(c => {
+        c.addEventListener('mouseover', () => {
+            c.style.backgroundColor = 'grey';
+        });
+        c.addEventListener('mouseout', () => {
+            c.style.backgroundColor = 'white';
+        });
+    });
+});
