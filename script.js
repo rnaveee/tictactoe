994846752e4e8a26a7b8e44e149093140c9dd5a9
Ryan Nave <ryannave97@gmail.com>
2026-04-02 14:50:15 -0700

    finished project

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ function Player(name, marker){
 }
 
 const grid = document.querySelector(".grid-container");
+const form = document.getElementById("playerForm");
+const playerOneInput = document.getElementById("playerOneName");
+const playerTwoInput = document.getElementById("playerTwoName");
 
+let currentMarker;
+let playerState = Boolean(Math.floor(Math.random() * 2));
+let winner = document.getElementById("winner");
+
+let playerOneShow;
+let playerTwoShow;
+let playerOne;
+let playerTwo;
+
+let gameState = false;
 
 let boxes = [];
 let j = 0;
@@ -15,14 +28,108 @@ for(let i = 0; i < 3; i++){
         boxRow.push(grid.children[j])
         j++;
     }
+    boxes.push(boxRow);
 }
 boxes.forEach(r => {
     r.forEach(c => {
         c.addEventListener('mouseover', () => {
-            c.style.backgroundColor = 'grey';
+            if(!c.classList.contains('clicked' && gameState)){
+                c.style.backgroundColor = 'grey';
+            }
         });
         c.addEventListener('mouseout', () => {
-            c.style.backgroundColor = 'white';
+            if(!c.classList.contains('clicked' && gameState)){
+                c.style.backgroundColor = 'white';
+            }
+        });
+        c.addEventListener('click', () => {
+            if(!c.classList.contains('clicked' && gameState)){
+                c.classList.add('clicked');
+                if(playerState){
+                    currentMarker = 'X'
+                    c.classList.add('X');
+                    playerTwoShow.style.color = 'green';
+                    playerOneShow.style.color = 'black';
+                } else {
+                    currentMarker = 'O';
+                    c.classList.add('O');
+                    playerOneShow.style.color = 'green';
+                    playerTwoShow.style.color = 'black';
+                }
+                c.textContent = currentMarker;
+                let check = checkWin(currentMarker);
+                if(check === currentMarker) {
+
+                    let win = document.getElementById("winner-container");
+                    win.style.visibility = 'visible';
+                    winner.style.fontWeight = 'bold';
+
+                    if(playerState) winner.textContent = playerOne.name;
+                    else winner.textContent = playerTwo.name;
+                }
+                playerState = !playerState;
+            }
         });
     });
 });
+
+form.addEventListener('submit', (formFunc) => {
+    formFunc.preventDefault();
+    gameState = true;
+
+    playerOne = new Player(playerOneInput.value, 'X');
+    playerTwo = new Player(playerTwoInput.value, 'O');
+
+    playerOneShow = document.getElementById("playerOneShow");
+    playerTwoShow = document.getElementById("playerTwoShow");
+    playerOneShow.textContent = playerOne.name;
+    playerTwoShow.textContent = playerTwo.name;
+    if(playerState){
+        currentMarker = 'X';
+        playerOneShow.style.color = 'green';
+    } else {
+        currentMarker = 'O';
+        playerTwoShow.style.color = 'green';
+    }
+});
+
+function checkWin(curr) {
+    console.log(curr);
+    let r= 0, c = 0;
+    if (
+        (boxes[r][c].classList.contains(curr) &&
+        boxes[r+1][c+1].classList.contains(curr) &&
+        boxes[r+2][c+2].classList.contains(curr)) ||
+
+        (boxes[r][c+2].classList.contains(curr) &&
+        boxes[r+1][c+1].classList.contains(curr) &&
+        boxes[r+2][c].classList.contains(curr)) ||
+
+        (boxes[r][c].classList.contains(curr) &&
+        boxes[r][c+1].classList.contains(curr) &&
+        boxes[r][c+2].classList.contains(curr)) ||
+
+        (boxes[r+1][c].classList.contains(curr) &&
+        boxes[r+1][c+1].classList.contains(curr) &&
+        boxes[r+1][c+2].classList.contains(curr)) ||
+
+        (boxes[r+2][c].classList.contains(curr) &&
+        boxes[r+2][c+1].classList.contains(curr) &&
+        boxes[r+2][c+2].classList.contains(curr)) ||
+
+        (boxes[r][c].classList.contains(curr) &&
+        boxes[r+1][c].classList.contains(curr) &&
+        boxes[r+2][c].classList.contains(curr)) ||
+
+        (boxes[r][c+1].classList.contains(curr) &&
+        boxes[r+1][c+1].classList.contains(curr) &&
+        boxes[r+2][c+1].classList.contains(curr)) ||
+
+        (boxes[r][c+2].classList.contains(curr) &&
+        boxes[r+1][c+2].classList.contains(curr) &&
+        boxes[r+2][c+2].classList.contains(curr))
+    ) {
+        return curr;
+    }
+    return '';
+}
